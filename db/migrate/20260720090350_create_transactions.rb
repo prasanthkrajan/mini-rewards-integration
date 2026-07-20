7a5c4fad8b56dc1cb699f2566c0740ac09070d17
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2026-07-20 17:39:44 +0800

    add transactions model and migrations

---
+++ db/migrate/20260720090350_create_transactions.rb
@@ -0,0 +1,16 @@
+class CreateTransactions < ActiveRecord::Migration[8.1]
+  def change
+    create_table :transactions do |t|
+      t.references :partner, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.decimal :points_delta, null: false
+      t.decimal :amount
+      t.string :kind, null: false
+      t.string :external_id, null: false
+      t.string :activity_type, null: false
+
+      t.timestamps
+    end
+    add_index :transactions, [:partner_id, :user_id, :external_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_20_084301) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_20_090350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,21 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_20_084301) do
     t.index ["api_key_digest"], name: "index_partners_on_api_key_digest", unique: true
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.string "activity_type", null: false
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.string "external_id", null: false
+    t.string "kind", null: false
+    t.bigint "partner_id", null: false
+    t.decimal "points_delta", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["partner_id", "user_id", "external_id"], name: "index_transactions_on_partner_id_and_user_id_and_external_id", unique: true
+    t.index ["partner_id"], name: "index_transactions_on_partner_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
@@ -29,4 +44,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_20_084301) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "transactions", "partners"
+  add_foreign_key "transactions", "users"
 end
