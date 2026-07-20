11a6b085eab536fca9ed9e483051cd4bb2064828
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2026-07-21 00:46:08 +0800

    fixed redirection to /login and /home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,19 +2,14 @@ class HomeController < ActionController::Base
   layout 'application'
 
   def root
-    # Client-side redirect logic handled by JavaScript
-    # For server-side preference: could check JWT validity here
     render 'home/root'
   end
 
   def login
-    # Render login page (sessions/new.html.erb)
     render 'sessions/new'
   end
 
   def index
-    # Render home page with rewards (home/index.html.erb)
-    # Stateless - no server-side validation needed
-    # Client-side JavaScript checks for JWT in localStorage
+    render 'home/index'
   end
 end
